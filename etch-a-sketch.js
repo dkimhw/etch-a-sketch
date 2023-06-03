e971281bb152abccbae77b0e1ceeef500a700e69
David Kim <dkhw90@gmail.com>
2023-06-03 02:30:28 +0200

    Add ability to reset etch a sketch

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,6 +2,7 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakeButton = document.querySelector('.shake');
+const MOVE_AMOUNT = 30;
 
 // Set up canvas
 const { width, height } = canvas;
@@ -12,7 +13,58 @@ ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 10;
 
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue} , 100%, 50%)`;
 ctx.beginPath();
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
+
+function draw({ key }) {
+  // update hue
+  hue += 5;
+  ctx.strokeStyle = `hsl(${hue} , 100%, 50%)`
+
+  // start the path
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+
+  // update x, y
+  switch (key) {
+    case 'ArrowUp':
+      y -= MOVE_AMOUNT;
+      break;
+    case 'ArrowDown':
+      y += MOVE_AMOUNT;
+      break;
+    case 'ArrowRight':
+      x += MOVE_AMOUNT;
+      break;
+    case 'ArrowLeft':
+      x -= MOVE_AMOUNT;
+      break;
+    default:
+      break;
+  }
+
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
+function handleKey(e) {
+  if (e.key.includes('Arrow')) {
+    e.preventDefault();
+    draw({ key: e.key });
+  }
+}
+
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener('animationend', function () {
+    canvas.classList.remove('shake');
+  }, { once: true });
+}
+
+window.addEventListener('keydown', handleKey);
+shakeButton.addEventListener('click', clearCanvas);
